1604eed1d4a6a779b3bc126d517ca1d9698f5a46
Saurabh9542 <saurabhverma9542@gmail.com>
2024-09-27 19:59:59 +0530

    Authorization and authentication added

--- database.py
+++ database.py
@@ -1,3 +1,6 @@
+import jwt
+from fastapi import HTTPException
+from datetime import datetime, timedelta
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
@@ -17,3 +20,43 @@ try:
     print("DB connected Success")
 except Exception as e:
     print("DB not connected", e)
+
+
+
+SECERET_KEY = "qwertyuiopasdfghjklzxcvbnm"
+ALGORITHM = "HS256"
+
+def create_access_token(data: dict, expires_delta: timedelta = None):
+    to_encode = data.copy()
+    if expires_delta:
+        expiry = datetime.utcnow() + expires_delta
+    else:
+        expiry = datetime.utcnow() + timedelta(minutes=15)
+    
+    to_encode.update({"exp": expiry})
+    encoded_jwt = jwt.encode(to_encode, SECERET_KEY, algorithm = ALGORITHM)
+    print(encoded_jwt)
+    return encoded_jwt
+
+
+def verify_access_token(token:str):
+    try:
+        payload = jwt.decode(token, SECERET_KEY, algorithms=[ALGORITHM])
+        user_id = payload.get("sub")
+        print(user_id)
+        if user_id is None:
+            raise HTTPException(status_code=401, detail="Invalid token")
+        return user_id
+
+    except jwt.PyJWTError:
+        raise HTTPException(status_code=401, detail="Invalid token")
+
+        
+
+# if __name__ == "__main__":
+    # data = {'user_id': '123', 'username': 'saurabh'}
+    # create_access_token(data)
+
+    # token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiMTIzIiwidXNlcm5hbWUiOiJzYXVyYWJoIiwiZXhwIjoxNzI3NDQ1NjM2fQ.GaA2kUc2M4SkGVr2d7v_zPmAT8PIxTvpfmNbkcy5DDk"
+    # verify_access_token(token)
+    
--- main.py
+++ main.py
@@ -1,15 +1,17 @@
-from fastapi import Depends, FastAPI, HTTPException, status
-from fastapi.responses import JSONResponse
-from sqlalchemy.orm import Session
-import crud
 import models
 import schemas
-from database import SessionLocal, engine
+import crud
+from fastapi import Depends, FastAPI, HTTPException, status
+from fastapi.responses import JSONResponse
+from fastapi.security import OAuth2PasswordBearer
+from sqlalchemy.orm import Session
+from database import SessionLocal, engine, create_access_token
 
 models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="login")
 
 # Dependency
 def get_db():
@@ -19,15 +21,23 @@ def get_db():
     finally:
         db.close()
 
+def get_current_user(token: str = Depends(oauth2_scheme), db: Session = Depends(get_db)):
+    user_id = verify_access_token(token)
+
+    user = db.query(models.User).filter(models.User.id == user_id).first()
+    if not user:
+        raise HTTPException(status_code=401, detail="User not found")
+    return user
+
 
 @app.get("/users/", response_model=list[schemas.SignUp])
-def read_users(db: Session = Depends(get_db)):
+def read_users(db: Session = Depends(get_db), current_user: models.User = Depends(get_current_user)):
     users = crud.get_users(db)
     return users
 
 
 @app.get("/users/{email}", response_model=schemas.SignUp)
-def read_user(email: str, db: Session = Depends(get_db)):
+def read_user(email: str, db: Session = Depends(get_db), current_user: models.User = Depends(get_current_user)):
     db_user = crud.get_user_by_email(db, email)
     if db_user is None:
         raise HTTPException(status_code=404, detail="User not found")
@@ -36,13 +46,13 @@ def read_user(email: str, db: Session = Depends(get_db)):
 
 @app.post("/users/{user_id}/posts/", response_model=schemas.Post)
 def create_item_for_user(
-    user_id: int, post: schemas.PostCreate, db: Session = Depends(get_db)
+    user_id: int, post: schemas.PostCreate, db: Session = Depends(get_db), current_user: models.User = Depends(get_current_user)
 ):
     return crud.create_user_post(db=db, post=post, user_id=user_id)
 
 
 @app.get("/users/{user_id}/posts", response_model = list[schemas.Post])
-def get_posts_by_user(user_id: int, db: Session = Depends(get_db)):
+def get_posts_by_user(user_id: int, db: Session = Depends(get_db), current_user: models.User = Depends(get_current_user)):
     get_posts = crud.get_posts_by_user(db, user_id)
     print(get_posts)
     if get_posts:
@@ -53,7 +63,7 @@ def get_posts_by_user(user_id: int, db: Session = Depends(get_db)):
 
 
 @app.get("/posts/", response_model=list[schemas.Post])
-def read_posts(db: Session = Depends(get_db)):
+def read_posts(db: Session = Depends(get_db), current_user: models.User = Depends(get_current_user)):
     items = crud.get_posts(db)
     return items
 
@@ -71,8 +81,12 @@ def signup_user(req: schemas.SignUp, db: Session = Depends(get_db)):
 @app.post("/login")
 def login_user(req: schemas.SignUp, db: Session = Depends(get_db)):
     check_creds = crud.login(db, req)
+    print(check_creds.id)
 
-    if check_creds:
-        return JSONResponse(status_code=200, content={"message": check_creds})
-    raise HTTPException(status_code=401, detail="Invalid login credentials")
+    if not check_creds:
+        raise HTTPException(status_code=401, detail="Invalid login credentials")
+
+    access_token = create_access_token(data = {"sub": str(check_creds.id)})
+    return {"access_token": access_token, "token_type": "bearer"}
+
 
